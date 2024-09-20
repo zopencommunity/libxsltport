e6f66a2f1d093f2dd167069f7d1082a34a92c97b
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:25 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/libxsltport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/libxsltport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libxsltport.git'), string(name: 'PORT_DESCRIPTION', value: 'libxslt is the XSLT C library' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libxsltport.git'), string(name: 'PORT_DESCRIPTION', value: 'libxslt is the XSLT C library' )]
   }
 }
